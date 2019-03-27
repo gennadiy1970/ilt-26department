acf30d138e29011e1b3fbf6ce8883d4810e87742
gennadiy1970 <ggm_kharkiv@ukr.net>
2019-03-27 12:29:48 +0200

    init

---
+++ pages/personal-pages/bogdan/bogdan_files/README.md
@@ -0,0 +1,3 @@
+# ilt-charkina.o.v.
+HTML + CSS only
+[view web-page](https://gennadiy1970.github.io/ilt-charkina.o.v./)
---
+++ pages/personal-pages/konstantinov/konstantinov_files/goTop.js
@@ -0,0 +1,18 @@
+function goTop() {
+    var
+        animationTime = 300,
+        framesCount = 20;
+
+    scroller = setInterval(function () {
+        let scrollBy = window.pageYOffset / framesCount;
+        if(window.pageYOffset > 0) {
+            window.scrollBy(0, -scrollBy);
+        } else {
+            clearInterval(scroller);
+        }
+    }, animationTime / framesCount);
+
+    window.onwheel = function() {
+        clearInterval(scroller);
+    };
+}
